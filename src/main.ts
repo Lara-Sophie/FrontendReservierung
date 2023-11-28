fad4643e0cade1950417b3311913c19f2f96d1a0
Lara-Sophie <Lara-Sophie.Lohdau@Student.HTW-Berlin.de>
2023-11-28 15:36:09 +0100

    verbindung

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './src/App.vue'
+//@ts-ignore
+import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min'
 
 const app = createApp(App)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <HelloWorld title="Wilkommen to your Vue.js App" />
   </main>
 </template>
+
+<script setup lang="ts">
+import HelloWorld from'../../FrontendReservierung/src/components/HelloWorld.vue'
+</script>
+
+<style scoped>
+
+</style>
